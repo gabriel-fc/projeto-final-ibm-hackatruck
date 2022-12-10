0c57ccd416150856b38be6d07a5000cfe7c82114
Student <student@student4.local>
2022-12-10 14:41:11 -0300

    first commit. project now is empty

---
+++ MedAlert/MedAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedAlert
+//
+//  Created by Student on 10/12/22.
+//  Copyright © 2022 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
